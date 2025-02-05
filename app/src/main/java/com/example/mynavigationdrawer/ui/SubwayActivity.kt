bbce98ede7d5115edacb27f6213282fa1d529a7f
pateeh <fatihazzam33@gmail.com>
2025-02-05 09:47:32 +0700

    membuat icon back bisa di click pada SubwayActivity

--- app/src/main/java/com/example/mynavigationdrawer/ui/SubwayActivity.kt
+++ app/src/main/java/com/example/mynavigationdrawer/ui/SubwayActivity.kt
@@ -1,21 +1,36 @@
 package com.example.mynavigationdrawer.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mynavigationdrawer.MainActivity
 import com.example.mynavigationdrawer.R
+import com.example.mynavigationdrawer.databinding.ActivitySubwayBinding
 
 class SubwayActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubwayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_subway)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySubwayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
